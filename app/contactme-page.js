bcb617acb8c0886d20174e2bd783ec2177fa581f
colbymcmanis <colbymcmanis01@gmail.com>
2023-11-01 11:14:16 -0500

    Button Submit Update

--- app/contactme-page.js
+++ app/contactme-page.js
@@ -18,3 +18,10 @@ export function onSkillsButtonTap(args) {
       page.frame.navigate("skills-page.xml");
       }
 
+    export function onSubmitButtonTap(args) {
+        const button = args.object;
+        const page = button.page;
+        page.frame.navigate("skills-page.xml");
+
+        page.getViewById('SubmissionSuccessful').text = "Form was Submitted Successfully. Colby will reach out to you as soon as possible. Thank you!"
+        }
