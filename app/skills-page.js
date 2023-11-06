4aff2e16c52e98bc61ffa24cc748362cbda09348
colbymcmanis <colbymcmanis01@gmail.com>
2023-11-06 11:07:32 -0600

    List View

--- app/skills-page.js
+++ app/skills-page.js
@@ -19,4 +19,18 @@ export function onContactMeButtonTap(args) {
       }
 
 
-    
+const ObservableArray = require("@nativescript/core/data/observable-array").ObservableArray;
+const listViewModule = require("@nativescript/core/ui/list-view");
+
+exports.pageLoaded = function(args) {
+    const page = args.object;
+  const listView = page.getViewById("myListView");
+
+  const itemList = new ObservableArray([
+  { name: 'Item 1' },
+  { name: 'Item 2' },
+  // ...
+]);
+listView.items = itemList;
+listView.itemTemplate = `<Label text="{{ name }}" />`;
+}
