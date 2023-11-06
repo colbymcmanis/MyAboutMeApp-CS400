18d994e36a48baedfff647d3604aece53470dc12
colbymcmanis <colbymcmanis01@gmail.com>
2023-11-06 11:52:45 -0600

    List view updated

--- app/skills-page.js
+++ app/skills-page.js
@@ -1,29 +1,11 @@
-import { createViewModel } from './main-view-model';
 
-export function onHomePageButtonTap(args) {
-  const button = args.object;
-  const page = button.page;
-  page.frame.navigate("main-page.xml");
-  }
-
-export function onAboutMeButtonTap(args) {
-    const button = args.object;
-    const page = button.page;
-    page.frame.navigate("Aboutme-page.xml");
-    }
-
-export function onContactMeButtonTap(args) {
-      const button = args.object;
-      const page = button.page;
-      page.frame.navigate("contactme-page.xml");
-      }
 
 
 const ObservableArray = require("@nativescript/core/data/observable-array").ObservableArray;
-const listViewModule = require("@nativescript/core/ui/list-view");
+
 
 exports.pageLoaded = function(args) {
-    const page = args.object;
+  const page = args.object;
   const listView = page.getViewById("myListView");
 
   const itemList = new ObservableArray([
@@ -32,5 +14,23 @@ exports.pageLoaded = function(args) {
   // ...
 ]);
 listView.items = itemList;
-listView.itemTemplate = `<Label text="{{ name }}" />`;
-}
+};
+
+
+exports.onHomePageButtonTap = function (args) {
+  const button = args.object;
+  const page = button.page;
+  page.frame.navigate("main-page.xml");
+  };
+
+exports.onAboutMeButtonTap = function (args) {
+    const button = args.object;
+    const page = button.page;
+    page.frame.navigate("Aboutme-page.xml");
+    };
+
+exports.onContactMeButtonTap = function (args) {
+      const button = args.object;
+      const page = button.page;
+      page.frame.navigate("contactme-page.xml");
+      };
